8789cdf800dc923b6d2152ef6809ed284063c6a4
esohi <esohi@proton.me>
2023-08-18 23:22:44 +0200

    initial commit

---
+++ first_jsx/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='to_do'>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul className='bucket'>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
